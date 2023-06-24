71282419e11b15f8b0d95e794256203d7914c90a
Scorias <mostafa.samy203@gmail.com>
2023-06-24 04:48:16 +0300

    fix

--- src/Traits/HasKernelValidations.php
+++ src/Traits/HasKernelValidations.php
@@ -4,6 +4,7 @@ namespace Volistx\Validation\Traits;
 
 use Volistx\Validation\Modules\AdminLogModule;
 use Volistx\Validation\Modules\PersonalTokensModule;
+use Volistx\Validation\Modules\PlanModule;
 use Volistx\Validation\Modules\SubscriptionModule;
 use Volistx\Validation\Modules\UserLogModule;
 use Volistx\Validation\Modules\UserModule;
@@ -19,6 +20,7 @@ trait HasKernelValidations
             "personal-tokens" => new PersonalTokensModule(),
             "admin-logs" => new AdminLogModule(),
             "user-logs" => new UserLogModule(),
+            "plans" => new PlanModule(),
             default => null,
         };
     }
