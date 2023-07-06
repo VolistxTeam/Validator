6912fd3d443f7dec548b1db2f8fbaa2faece932e
Cryental <contact@cryental.dev>
2023-07-06 00:08:37 +0000

    fix config problem

--- src/ValidationProvider.php
+++ src/ValidationProvider.php
@@ -3,6 +3,7 @@
 namespace Volistx\Validation;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class ValidationProvider extends ServiceProvider
 {
@@ -13,10 +14,32 @@ class ValidationProvider extends ServiceProvider
      */
     public function register(): void
     {
+        if ($this->app->runningInConsole()) {
+            $this->registerResources();
+        }
+
+        if ($this->isLumen() === false) {
+            $this->mergeConfigFrom(__DIR__.'/../config/volistx-validation.php', 'volistx-validation');
+        }
+
         $this->loadJsonTranslationsFrom(__DIR__ . '/../lang');
 
         $this->publishes([
             __DIR__ . '/../lang' => resource_path('lang/vendor/validator'),
         ]);
+    }
+
+    public function registerResources()
+    {
+        if ($this->isLumen() === false) {
+            $this->publishes([
+                __DIR__.'/../config/volistx-validation.php' => config_path('volistx-validation.php'),
+            ], 'config');
+        }
+    }
+
+    protected function isLumen()
+    {
+        return Str::contains($this->app->version(), 'Lumen') === true;
     }
 }
